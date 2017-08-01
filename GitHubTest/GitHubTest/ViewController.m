145ccc764575fe669fa89a5b5f3d6056206db262
zhanghui1830 <zhanghui1830@163.com>
2017-08-01 15:04:44 +0800

    Log

--- GitHubTest/GitHubTest/ViewController.m
+++ GitHubTest/GitHubTest/ViewController.m
@@ -18,6 +18,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"%@",@"1234");
+    NSLog(@"%@",@"1234");
+    NSLog(@"%@",@"1234");
 }
 
 - (void)didReceiveMemoryWarning {
