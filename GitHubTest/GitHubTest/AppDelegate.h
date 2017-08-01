6db0e4b6d5a81cced5e092d474ef6b31a09ede0d
zhanghui1830 <zhanghui1830@163.com>
2017-08-01 15:01:13 +0800

    KyleTest

---
+++ GitHubTest/GitHubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubTest
+//
+//  Created by Kyle on 2017/8/1.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubTest/GitHubTest/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  GitHubTest
+//
+//  Created by Kyle on 2017/8/1.
+//  Copyright © 2017年 lyj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"%@",@"1234");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
